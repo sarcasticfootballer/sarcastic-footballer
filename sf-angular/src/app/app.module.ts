b79198db43e85d5287124c506764db1cae6af892
pishdude <hariarun.19@gmail.com>
2018-02-27 22:30:15 +0530

    Routing for nav bar

--- sf-angular/src/app/app.module.ts
+++ sf-angular/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DataHandlerService } from './services/data-handler.service';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
+import {Routes,RouterModule} from '@angular/router'
+
+const varun : Routes =[
+{ path :'Home' ,component : HomeComponent },
+{ path :'Author' ,component : AuthorsComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(varun)
   ],
   providers: [DataHandlerService],
   bootstrap: [AppComponent]
