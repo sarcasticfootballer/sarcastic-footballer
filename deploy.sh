2180b302a926bfcb7e859948c931c906ee303335
msreetheja <msreetheja@gmail.com>
2018-04-07 23:21:50 +0530

    fixed appengine deploy error

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -r dist
 cd sf-angular
 ng build
+cp -r dist ../
 cd ..
 gcloud app deploy --version=1
--- testlocal.sh
+++ testlocal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -r dist
 cd sf-angular
 ng build
+cp -r dist ../
 cd ..
 dev_appserver.py .
